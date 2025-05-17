a183b5d0a0134b457c22b84f4c913546ea342e7c
Freddy <freddypalencia125@gmail.com>
2025-05-17 11:56:32 -0400

    Actualiza las dependencias del proyecto: se añade `vue-router` a `package.json` y se ajusta el comando de construcción para omitir la verificación de tipos. Se excluyen archivos de prueba en la configuración de `vite` y se mejora la configuración de `tsconfig` para excluir archivos de prueba. Se añaden plugins de JSX y herramientas de desarrollo de Vue en la configuración de Vite.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    vueJsx(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    // Excluir archivos de test durante el build
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      },
+      external: ['**/*.spec.*', '**/*.test.*', '**/vitest.setup.*', '**/tests/**']
+    }
+  }
 })
